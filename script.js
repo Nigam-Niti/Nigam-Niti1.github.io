d9448c6e3b43605896a8dd6a7c0176cc0eeaebcd
Nitika <58980784+Nigam-Niti@users.noreply.github.com>
2023-01-06 10:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('models')
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
 async function start() {
@@ -36,12 +36,12 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Khushboo', 'Monika']
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage('labeled_images1/${label}/${i}.jpg')
+        const img = await faceapi.fetchImage('labeled_images/${label}/${i}.jpg')
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }

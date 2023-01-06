3618aca5a0e8f8a5cf703bfef7e5ec92f7bb5cd4
Nitika <58980784+Nigam-Niti@users.noreply.github.com>
2023-01-06 11:10:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+
 const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('models')
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
 async function start() {
@@ -41,7 +42,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage('labeled_images/${label}/${i}.jpg')
+        const img = await faceapi.fetchImage(`labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
